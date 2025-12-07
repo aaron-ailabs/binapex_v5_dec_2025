9a703ba752bf6ad7117154a24607d4b011804ffe
Aaron Labs <aaron.ailabs@gmail.com>
2025-12-07 02:25:38 +0000

    build

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{fontFamily: 'system-ui, sans-serif', padding: 24}}>
+      <h1>binapex_v5_dec (Vite + React + TypeScript)</h1>
+      <p>Build succeeded.</p>
+    </div>
+  )
+}
